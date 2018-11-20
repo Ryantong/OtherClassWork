caad79eb39bf59c68fbed30677150f7ef5861fa0
Ryantong <20tongr31@mymhs.org>
2018-11-20 09:53:45 -0800

    update

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -9,7 +9,7 @@ public class FracCalc {
     	Scanner input = new Scanner(System.in);
     	String temp = input.next();
     	while(!temp.equals("quit")) {
-    		produceAnswer(temp);
+    		System.out.println(produceAnswer(temp));
     		//print result
     		temp = input.next();
     	}
@@ -32,18 +32,32 @@ public class FracCalc {
     	String Operator = arr[1];
     	String secondOp = arr[2];
     	
-    	String[] arr1 = firstOp.split("_");
-    	int wholeNum1 = Integer.parseInt(arr1[0]);
-    	String[] temp = arr1[1].split("/");
-    	int numer1 = Integer.parseInt(temp[0]);
-    	int denom1 = Integer.parseInt(temp[1]);
+    	return secondOp;
     	
-    	String[] arr2 = secondOp.split("_");
-    	int wholeNum2 = Integer.parseInt(arr2[0]);
+//    	String[] arr1 = firstOp.split("_");
+//    	int wholeNum1 = Integer.parseInt(arr1[0]);
+//    	String[] temp1 = arr1[1].split("/");
+//    	int numer1 = Integer.parseInt(temp1[0]);
+//    	int denom1 = Integer.parseInt(temp1[1]);
+//    	int mixedFrac1 = toImproperFrac(wholeNum1, numer1, denom1);
+//    	
+//    	String[] arr2 = secondOp.split("_");
+//    	int wholeNum2 = Integer.parseInt(arr2[0]);
+//    	String[] temp2 = arr2[1].split("_");
+//    	int numer2 = Integer.parseInt(temp2[0]);
+//    	int denom2 = Integer.parseInt(temp2[1]);
+//    	int mixedFrac2 = toImproperFrac(wholeNum2, numer2, denom2);
+//    	return "whole:" + wholeNum2 + " numerator:" + numer2 + " denominator:" + denom2;
+
     	
     	
         // TODO: Implement this function to produce the solution to the input
     }
+	public static int toImproperFrac(int wholeNum, int numer, int denom) {
+		int fracNumer = denom * wholeNum + numer;
+		return fracNumer;
+	}
+    
 
     // TODO: Fill in the space below with any helper methods that you think you will need
     
